298aa67f2fb2a8ed6b7f3e35f8c4265a2d47c13b
wahib.adiyatma <wahib.adiyatma@ui.ac.id>
2021-03-31 19:52:01 +0700

    fix minor error

--- store/forms.py
+++ store/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from store.models import Penjual
-from store.models import Barang
 from django.db import migrations, models
 
 #To Do : Make form by using ModelForm
--- store/views.py
+++ store/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Penjual,Barang
-from .forms import tambahBarangForm, tambahPenjualForm
 
 
 # Create your views here.
 # To Do : Create a function for showing the index, submit form for tambah_barang, and submit form fortambah_penjual
+def index():
+	return render(request,"store/index.html")
